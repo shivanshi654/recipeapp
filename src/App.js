2f43ec78c323b11b3e44adb43f2d9b3dfb9aa78c
Shivanshi <1999shivanshi.patel@gmail.com>
2021-01-11 17:51:20 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react';
+import './App.css';
+import Header from './components/Header';
+import Recipes from './components/Recipes';
+import Axios from 'axios';
+
+function  App() {
+  const [search,setSearch]=useState("chicken");
+  const[recipes,setRecipes]=useState([]);
+  const APP_ID="f775cc97";
+  const APP_KEY="1a25aa3ed52977e72250acbc95d7b63e";
+
+  useEffect( () =>{
+    getRecipes();
+  },[]);
+  const getRecipes=async()=>{
+    const res=await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+    setRecipes(res.data.hits);
+  };
+  const onInputChange=e=>{
+    setSearch(e.target.value)
+  };
+  const onSearchClick=()=>{
+    getRecipes();
+  }
+  return (
+    <div className="App">
+      <Header 
+      search={search} 
+      onInputChange={onInputChange}
+      onSearchClick={onSearchClick}/>
+      <div className="container">
+      <Recipes recipes={recipes}/>   
+    </div>
+    </div>
+  );
+}
+
+export default App;
