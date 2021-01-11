a8b00d3b5f802bdd0f0699a1b788a9c1971ee836
Shivanshi <1999shivanshi.patel@gmail.com>
2021-01-11 17:56:49 +0530

    Updates

---
+++ precache-manifest.01071e62568d3a5486b66ba509fd92ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7124ff06118eb4cd1f1949cd188f21",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "3a6f088eee64d0ef9579",
+    "url": "/recipeapp/static/css/main.34a59236.chunk.css"
+  },
+  {
+    "revision": "62d86195652e1f135048",
+    "url": "/recipeapp/static/js/2.373826eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.373826eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6f088eee64d0ef9579",
+    "url": "/recipeapp/static/js/main.4cec1545.chunk.js"
+  },
+  {
+    "revision": "49ebba55aa4b97f15501",
+    "url": "/recipeapp/static/js/runtime-main.b9484471.js"
+  }
+]);
